97e6175a4c268c576180aba3cec1307ad6539c24
noeszc <noeszc@gmail.com>
2024-11-23 20:01:05 -0600

    feat(nvim): enhance editor dx

--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,77 +1,97 @@
 return {
-  -- Highlight todo, notes, etc in comments
+  -- Todo Comments: Highlight special comments like TODO, NOTE, etc.
+  -- Provides visual distinction for important notes in code comments
   {
     "folke/todo-comments.nvim",
-    event = "VimEnter",
+    event = "VimEnter", -- Load when Neovim starts
     dependencies = { "nvim-lua/plenary.nvim" },
-    opts = { signs = false },
+    opts = { signs = false }, -- Disable gutter signs
   },
+
+  -- Comment.nvim: Smart code commenting
+  -- Provides commands to comment/uncomment code blocks
   {
     "numToStr/Comment.nvim",
     keys = {
-      { "gc", mode = { "n", "v" }, "gcc" },
+      { "gc", mode = { "n", "v" }, "gcc" }, -- Toggle comments with 'gc' in normal/visual mode
     },
     opts = {
+      -- Integration with ts-context-commentstring for language-aware commenting
       pre_hook = function(ctx)
         return require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook()(ctx)
       end,
     },
   },
+
+  -- Context Commentstring: Language-aware comment strings
+  -- Ensures correct comment syntax in different contexts (e.g., JSX/TSX)
   {
     "JoosepAlviste/nvim-ts-context-commentstring",
-    ft = { "typescriptreact" },
+    ft = { "typescriptreact" }, -- Load only for TypeScript React files
     opts = {
-      enable_autocmd = false,
+      enable_autocmd = false, -- Disable automatic loading
     },
   },
+
+  -- TS Autotag: Automatically close HTML/JSX tags
+  -- Updates closing tags when editing opening tags and vice versa
+  { "windwp/nvim-ts-autotag", event = "InsertEnter", opts = {} },
+
+  -- Autopairs: Smart bracket/quote pairing
+  -- Automatically inserts closing brackets, quotes, etc.
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter", -- Load when entering insert mode
+    dependencies = { "hrsh7th/nvim-cmp" }, -- Optional CMP integration
+    config = function()
+      require("nvim-autopairs").setup({})
+      -- Add parentheses after completing function or method
+      local cmp_autopairs = require("nvim-autopairs.completion.cmp")
+      local cmp = require("cmp")
+      cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done())
+    end,
+  },
+
+  -- Template String: Smart template string conversion
+  -- Automatically converts quotes to template strings when needed
   {
     "axelvc/template-string.nvim",
     opts = {
       filetypes = { "typescript", "javascript", "typescriptreact", "javascriptreact", "vue" },
-      remove_template_string = true,
+      remove_template_string = true, -- Remove template string when no interpolation
       restore_quotes = {
-        normal = [[']],
-        jsx = [["]],
+        normal = [[']], -- Single quotes for normal strings
+        jsx = [["]], -- Double quotes for JSX attributes
       },
     },
     event = "InsertEnter",
     ft = { "typescript", "javascript", "typescriptreact", "javascriptreact", "vue" },
   },
-  { -- Collection of various small independent plugins/modules
+
+  -- Mini.nvim: Collection of independent Neovim plugins
+  -- Provides various text editing enhancements
+  {
     "echasnovski/mini.nvim",
     config = function()
-      -- Better Around/Inside textobjects
-      --
-      -- Examples:
-      --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [Q]uote
-      --  - ci'  - [C]hange [I]nside [']quote
-      require("mini.ai").setup({ n_lines = 500 })
+      -- Mini.ai: Enhanced text objects
+      -- Provides smarter around/inside operations
+      require("mini.ai").setup({ n_lines = 500 }) -- Limit search to 500 lines
 
-      -- Add/delete/replace surroundings (brackets, quotes, etc.)
-      --
-      -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-      -- - sd'   - [S]urround [D]elete [']quotes
-      -- - sr)'  - [S]urround [R]eplace [)] [']
+      -- Mini.surround: Surround text operations
+      -- Adds, deletes, or replaces surrounding characters
       require("mini.surround").setup()
 
-      -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
+      -- Mini.statusline: Minimal status line
+      -- Provides essential information in a clean format
       local statusline = require("mini.statusline")
-      -- set use_icons to true if you have a Nerd Font
       statusline.setup({ use_icons = vim.g.have_nerd_font })
 
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
+      -- Customize statusline location section
+      -- Shows cursor position as LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
         return "%2l:%-2v"
       end
-
-      -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
 }
